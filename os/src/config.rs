d75eda637526fcb7c3794d1276daea3c62986a7b
wzy <2049562063@qq.com>
2025-06-05 22:50:57 +0800

    enlarge user stack size

--- os/src/config.rs
+++ os/src/config.rs
@@ -1,6 +1,6 @@
 #[allow(unused)]
 
-pub const USER_STACK_SIZE: usize = 4096 * 2;
+pub const USER_STACK_SIZE: usize = 4096 * 2 * 1024;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_HEAP_SIZE: usize = 0x100_0000;
 pub const PAGE_SIZE: usize = 0x1000;
